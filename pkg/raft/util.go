a9d6d61a9a5b0d1a17fc37a64c1aa5b0d7ac45ae
niziolekpaul <niziolek.paul@gmail.com>
2024-12-07 20:32:13 -0500

    randomTimeout utility function for heartbeats

--- pkg/raft/util.go
+++ pkg/raft/util.go
@@ -1,6 +1,10 @@
 package raft
 
-import "log"
+import (
+	"log"
+	"math/rand"
+	"time"
+)
 
 // Debugging
 const Debug = 0
@@ -10,4 +14,13 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 		log.Printf(format, a...)
 	}
 	return
+}
+
+// returns a channel with time duration between minVal and 2x minVal
+func randomTimeout(minVal time.Duration) <-chan time.Time {
+	if minVal == 0 {
+		return nil
+	}
+	extra := time.Duration(rand.Int63()) % minVal
+	return time.After(minVal + extra)
 }
